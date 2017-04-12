6e3636d3652531641ad4c493a456c6990b967d37
VadimSquorpikkor <vadimserikov11@gmail.com>
2017-04-12 16:10:29 +0300

    5

--- 2/2_6_week_exam/2_stroke.py
+++ 2/2_6_week_exam/2_stroke.py
@@ -11,12 +11,25 @@
 # Sample Output:
 # 1 2 2 3 3 3 4
 
-# n = int(input())
-n = 7
-count = 1
+n = int(input())
+# n = int(15)
+x = int(1)
 stroke = ''
-while count <= n:
-    stroke += (str(count)+' ')*count
-    count += 1
+length = int(0)
+while n > 0:
+    length = x
+    while length > 0 and n > 0:
+        stroke += str(x) + ' '
+        length -= 1
+        n -= 1
+    x += 1
 print(stroke)
 
+
+"""x = int(input())
+lst = []
+for k in range(x+1):
+    b = [k]
+    lst+= b * k
+print(' '.join(str(i) for i in lst[:x]))
+"""
