aa451030bd4076c15724a816156341a024207c54
VadimSquorpikkor <vadimserikov11@gmail.com>
2017-04-13 14:43:15 +0300

    5

--- 2/2_6_week_exam/4_crossroad.py
+++ 2/2_6_week_exam/4_crossroad.py
@@ -29,45 +29,6 @@
 #
 # 4
 
-"""# inputMatrix = []
-# inputMatrix.append([int(x) for x in input().split()])
-# inputMatrix = [[9,5,3],[0,7,-1],[-5,2,9]]
-inStr = str(input())
-stroke = ''
-# inputMatrix = [1, 2, 3, 4, 5]
-# stroke = ''.join(str(x) for x in inputMatrix)
-while inStr != 'end':
-    stroke += inStr + ' '
-    inStr = str(input())
-# stroke = [int(x) for x in ]
-print(stroke)"""
-
-"""outStroke = []
-a = 0
-b = 0
-stroke = []
-inStr = str(input())
-while inStr != 'end':
-    b += 1
-    temp = [int(x) for x in inStr.split()]
-    a = int(len(temp))
-    stroke += temp
-    inStr = str(input())
-
-# stroke = [4, 1, 3, 2]
-for i in range(a * b):
-    outStroke += [int(stroke[i - 1]) + int(stroke[i + 1 - a * b]) + int(stroke[i - a]) + int(stroke[i - a * b + a])]
-for i in range(0, a * b, a):
-    for j in range(i, i + a):
-        print(outStroke[j], end=' ')
-    print('')
-print(stroke)
-print(outStroke)
-print(stroke[-1])
-print(a)
-print(b)"""
-
-outStroke = []
 a = 0
 b = 0
 stroke = []
@@ -79,14 +40,8 @@ while inStr != 'end':
     stroke.append(temp)
     inStr = str(input())
 
-# stroke = [4, 1, 3, 2]
 for i in range(b):
-    outStroke += []
     for j in range(a):
-        # outStroke[i] += [int(stroke[i][j - 1]) + int(stroke[i][j - a + 1]) + int(stroke[i - 1][j]) + int(stroke[i - b + 1][j])]
-        outStroke[i] += [3]
-for i in range(0, a * b, a):
-    for j in range(i, i + a):
-        print(outStroke[j], end=' ')
+        crossSum = stroke[i][j-1]+stroke[i][j+1-a]+stroke[i-1][j]+stroke[i+1-b][j]
+        print(crossSum, end=' ')
     print('')
-
