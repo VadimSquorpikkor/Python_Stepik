2940b5aeac49bdd3a0aec84687206e2195615fb0
VadimSquorpikkor <vadimserikov11@gmail.com>
2017-04-13 16:51:30 +0300

    6

--- 2/2_6_week_exam/5_spiral.py
+++ 2/2_6_week_exam/5_spiral.py
@@ -14,3 +14,25 @@
 # 14 23 22 21 8
 # 13 12 11 10 9
 
+n = int(5)
+matrix = [[0]*n for i in range(n)]
+print(matrix)
+startX = int(0)
+startY = int(0)
+stopX = int(n)
+stopY = int(n)
+stepX = int(1)
+stepY = int(1)
+count = 1
+x = 0
+y = 0
+while True:
+    for y in range(startY, stopY, stepY):
+        matrix[x][y] = count
+        count += 1
+    stepY = -1
+    startY = stopX - 1
+    stopY = startX + 1
+
+
+print(matrix)
