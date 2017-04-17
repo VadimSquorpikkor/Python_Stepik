5a28b342d8a9eebab6f523e9ff4c3eb181fb946b
VadimSquorpikkor <vadimserikov11@gmail.com>
2017-04-17 16:10:21 +0300

    7

--- 2/temp.py
+++ 2/temp.py
@@ -1,4 +1,10 @@
-res = [(x + 2) ** 2 for x in range(4, 13, 5) if x % 3 != 0]
-for i in res:
-    print(i)
-print(res)
+# res = [(x + 2) ** 2 for x in range(4, 13, 5) if x % 3 != 0]
+# for i in res:
+#     print(i)
+# print(res)
+
+import math
+
+print(math.pi)
+
+
